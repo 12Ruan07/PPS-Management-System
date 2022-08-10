007d5fa69a871fd85a972d09e30709a2ee428e96
Ruan Burger <91562022+12Ruan07@users.noreply.github.com>
2022-08-10 12:58:21 +0200

    Update PPS_Engineering_Database.sql
    Added functionality to drop tables.
    Fixed Primary Key constraints.
    Fixed Primary Key violations

--- PPS_Engineering_Database.sql
+++ PPS_Engineering_Database.sql
@@ -1,12 +1,19 @@
 CREATE DATABASE PPS_Database;
 
+DROP TABLE EMPLOYEE;
+DROP TABLE CLIENT;
+DROP TABLE COMPANY;
+DROP TABLE PHASE;
+DROP TABLE PROJECT;
+
+
 CREATE TABLE EMPLOYEE (
 Employee_ID char(13) NOT NULL,
 FirstName varchar(55) NOT NULL,
 LastName varchar(55) NOT NULL,
 Username char(10) NOT NULL,
 Password char(10) NOT NULL,
-PRIMARY KEY (Employee_ID)
+CONSTRAINT Employee_ID_PK PRIMARY KEY (Employee_ID)
 );
 
 CREATE TABLE CLIENT (
@@ -14,31 +21,32 @@ Client_ID char(13) NOT NULL,
 FirstName varchar(55) NOT NULL,
 LastName varchar(55) NOT NULL,
 Telephone_Number char(10) NOT NULL,
-PRIMARY KEY (Client_ID)
+CONSTRAINT Client_ID_PK PRIMARY KEY (Client_ID)
 );
 
 CREATE TABLE COMPANY (
+Company_ID int NOT NULL,
 Company_Name varchar(55) NOT NULL,
 Company_Address varchar(55) NOT NULL,
-PRIMARY KEY (Company_Name)
+CONSTRAINT Company_ID PRIMARY KEY (Company_ID)
 );
 
 CREATE TABLE PHASE (
 Project_ID int NOT NULL,
 Project_Phase varchar(35),
-PRIMARY KEY (Project_ID)
+CONSTRAINT Project_ID_PK PRIMARY KEY (Project_ID)
 );
 
 CREATE TABLE PROJECT (
 Project_ID int NOT NULL,
 Employee_ID char(13), 
 Client_ID char(13), 
-Company_Name varchar(55) NOT NULL, 
+Company_ID int NOT NULL, 
 Project_Phase varchar(35),
 Project_Date date,
 Project_Price money,
 PRIMARY KEY (Project_ID),
 FOREIGN KEY (Employee_ID) REFERENCES EMPLOYEE(Employee_ID),
 FOREIGN KEY (Client_ID) REFERENCES CLIENT(Client_ID),
-FOREIGN KEY (Company_Name) REFERENCES COMPANY(Company_Name)
+FOREIGN KEY (Company_ID) REFERENCES COMPANY(Company_ID)
 );
